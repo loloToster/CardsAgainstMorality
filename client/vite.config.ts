285e17e54c41aea0e01cc4e3c69a55dbe488c88a
loloToster <lolo.toster4@gmail.com>
2023-03-16 11:48:28 +0100

    🔧 updated configuration files

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,5 +3,13 @@ import vue from "@vitejs/plugin-vue"
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      "/auth": {
+        target: "http://localhost:3000/",
+        secure: false
+      }
+    }
+  },
   plugins: [vue()]
 })
