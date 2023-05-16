14d3f6acc66244b51d6b279a475cec6b0d1a3993
loloToster <lolo.toster4@gmail.com>
2023-05-16 18:58:52 +0200

    💄 further work on pack search

--- client/src/pages/PacksPage.vue
+++ client/src/pages/PacksPage.vue
@@ -1,19 +1,33 @@
 <script setup lang="ts">
 import { reactive, ref } from "vue"
 import { RouterLink } from "vue-router"
-import { ApiCardPack } from "@backend/types"
-import AppLoading from "@/components/AppLoading.vue"
-import CardPack from "@/components/CardPack.vue"
 import { onClickOutside } from "@vueuse/core"
+
+import { ApiCardPack } from "@backend/types"
+
+import AppLoading from "@/components/AppLoading.vue"
+import AppButton from "@/components/AppButton.vue"
+import CardPack from "@/components/CardPack.vue"
+
+const SORT_TYPES = {
+  likes: "Likes",
+  cards: "Number of Cards",
+  blacks: "Number of Black Cards",
+  whites: "Number of White Cards"
+}
+
+type SortType = keyof typeof SORT_TYPES
 
 const state = reactive<{
   searchQuery: string
+  sortBy: null | SortType
   searchAdditionalActive: boolean
   sortDropdownActive: boolean
   loading: boolean
   packs: ApiCardPack[]
 }>({
   searchQuery: "",
+  sortBy: null,
   searchAdditionalActive: false,
   sortDropdownActive: false,
   loading: true,
@@ -28,7 +42,30 @@ fetch("/api/packs").then(async res => {
   }
 })
 
+function handleSort(sortType: SortType) {
+  state.sortDropdownActive = false
+
+  if (state.sortBy === sortType) {
+    state.sortBy = null
+  } else {
+    state.sortBy = sortType
+  }
+}
+
+const searchInputWrapper = ref<HTMLDivElement>()
 const searchQueryInput = ref<HTMLInputElement>()
+
+function handleInputWrapperClick() {
+  if (!state.searchAdditionalActive) {
+    state.searchAdditionalActive = true
+    searchQueryInput.value?.focus()
+  }
+}
+
+onClickOutside(searchInputWrapper, () => {
+  state.searchAdditionalActive = false
+})
+
 const sortSection = ref<HTMLDivElement>()
 
 onClickOutside(sortSection, () => {
@@ -40,10 +77,12 @@ onClickOutside(sortSection, () => {
   <div class="packs-page">
     <div class="search">
       <div
+        @click="handleInputWrapperClick"
+        ref="searchInputWrapper"
         class="search__input-wrapper"
         :class="{ active: state.searchAdditionalActive }"
       >
-        <div @click="searchQueryInput?.focus()" class="search__main-input">
+        <div class="search__main-input">
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 96 960 960">
             <path
               d="M795.761 941.696 531.326 677.5q-29.761 25.264-69.6 39.415-39.84 14.15-85.161 14.15-109.835 0-185.95-76.195Q114.5 578.674 114.5 471t76.196-183.87q76.195-76.195 184.369-76.195t183.87 76.195q75.695 76.196 75.695 184.02 0 43.328-13.641 82.97-13.641 39.641-40.924 74.402L845.5 891.957l-49.739 49.739ZM375.65 662.935q79.73 0 135.29-56.245Q566.5 550.446 566.5 471t-55.595-135.69q-55.595-56.245-135.255-56.245-80.494 0-136.757 56.245Q182.63 391.554 182.63 471t56.228 135.69q56.227 56.245 136.792 56.245Z"
@@ -51,8 +90,6 @@ onClickOutside(sortSection, () => {
           </svg>
           <input
             v-model="state.searchQuery"
-            @focus="state.searchAdditionalActive = true"
-            @focusout="state.searchAdditionalActive = false"
             ref="searchQueryInput"
             class="search__input"
             type="search"
@@ -78,22 +115,26 @@ onClickOutside(sortSection, () => {
         <button
           @click="state.sortDropdownActive = !state.sortDropdownActive"
           class="search__sort__open"
-          :class="{ active: state.sortDropdownActive }"
+          :class="{ open: state.sortDropdownActive, active: state.sortBy }"
         >
-          <span>Sort</span>
+          <span v-if="state.sortBy">
+            Sort by: {{ SORT_TYPES[state.sortBy] }}
+          </span>
+          <span v-else>Sort</span>
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 96 960 960">
             <path d="M480 696 280 497h400L480 696Z" />
           </svg>
         </button>
-        <div
-          v-if="state.sortDropdownActive"
-          @click="console.log('test')"
-          class="search__sort-dropdown"
-        >
-          <button>Likes</button>
-          <button>Number of Cards</button>
-          <button>Number of Black Cards</button>
-          <button>Number of White Cards</button>
+        <div v-if="state.sortDropdownActive" class="search__sort-dropdown">
+          <AppButton
+            v-for="(value, key) in SORT_TYPES"
+            @click="handleSort(key)"
+            class="search__sort-dropdown__btn"
+            :class="{ active: key === state.sortBy }"
+            :key="key"
+          >
+            {{ value }}
+          </AppButton>
         </div>
       </div>
     </div>
@@ -114,6 +155,7 @@ onClickOutside(sortSection, () => {
 </template>
 
 <style scoped lang="scss">
+@use "@/styles/variables" as vars;
 @use "@/styles/mixins" as mixins;
 @use "@/styles/colors" as colors;
 
@@ -126,7 +168,8 @@ onClickOutside(sortSection, () => {
   $gap: 8px;
   $roundness: 6px;
 
-  position: relative;
+  position: sticky;
+  top: calc(vars.$header-height + 20px);
   z-index: 1;
 
   display: flex;
@@ -141,13 +184,20 @@ onClickOutside(sortSection, () => {
     fill: currentColor;
   }
 
-  &__input-wrapper {
-    position: relative;
-    flex-grow: 1;
-    display: flex;
-    align-items: center;
+  @mixin search-surface() {
     background-color: colors.$inp-bg;
     border-radius: $roundness;
+    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.3);
+  }
+
+  &__input-wrapper {
+    @include search-surface();
+
+    position: relative;
+    flex-grow: 1;
+    min-width: 0;
+    display: flex;
+    align-items: center;
 
     &.active {
       border-radius: $roundness $roundness 0 0;
@@ -163,10 +213,12 @@ onClickOutside(sortSection, () => {
     svg {
       width: 24px;
       height: 24px;
+      flex-shrink: 0;
     }
 
     input {
       flex-grow: 1;
+      min-width: 0;
       font-size: inherit;
 
       &::placeholder {
@@ -198,10 +250,10 @@ onClickOutside(sortSection, () => {
   &__sort {
     position: relative;
     white-space: nowrap;
-    background-color: colors.$inp-bg;
-    border-radius: $roundness;
 
     &__open {
+      @include search-surface();
+
       display: flex;
       align-items: center;
       gap: 6px;
@@ -210,29 +262,44 @@ onClickOutside(sortSection, () => {
       font-size: inherit;
       cursor: pointer;
 
+      &.active {
+        $outline: 2px;
+        outline: $outline colors.$primary solid;
+        outline-offset: -$outline;
+        background-color: darken(colors.$primary, 10%);
+      }
+
       svg {
         width: 24px;
         height: 24px;
         transition: transform 300ms;
       }
 
-      &.active svg {
+      &.open svg {
         transform: rotate(180deg);
       }
     }
   }
 
   &__sort-dropdown {
+    @include search-surface();
+
     display: flex;
     flex-direction: column;
-  }
-
-  &__sort-dropdown {
+    gap: 6px;
     position: absolute;
     right: 0;
     top: calc(100% + $gap);
-    background-color: colors.$inp-bg;
-    border-radius: $roundness;
+    padding: 6px;
+
+    &__btn {
+      --color: transparent;
+      text-align: start;
+
+      &.active {
+        --color: unset;
+      }
+    }
   }
 }
 
